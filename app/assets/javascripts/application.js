29288ade3c89242da3c9646c5cba5ab3284ec71e
Amit Gupta <amit.gupta@quovantis.com>
2013-06-20 02:39:10 +0530

    Jquery slider changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,36 +14,3 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
-
-
-$(function() {
-	var select = $( "#instance-cpu" );
-	var slider = $( "<div id='slider'></div>" ).insertAfter( select ).slider({
-		min: 1,
-		max: 5,
-		range: "min",
-		value: select[ 0 ].selectedIndex + 1,
-		slide: function( event, ui ) {
-			select[ 0 ].selectedIndex = ui.value - 1;
-		}
-	});
-	$( "#instance-cpu" ).change(function() {
-		slider.slider( "value", this.selectedIndex + 1 );
-	});
-});
-
-$(function() {
-	var select = $( "#instance-ram" );
-	var slider = $( "<div id='slider'></div>" ).insertAfter( select ).slider({
-		min: 1,
-		max: 5,
-		range: "min",
-		value: select[ 0 ].selectedIndex + 1,
-		slide: function( event, ui ) {
-			select[ 0 ].selectedIndex = ui.value - 1;
-		}
-	});
-	$( "#instance-ram" ).change(function() {
-		slider.slider( "value", this.selectedIndex + 1 );
-	});
-});
