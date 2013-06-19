0a85bea956b68949fe0beddc5889d7444e293547
Siddhant <siddhant.bhardwaj@quovantis.com>
2013-06-19 18:07:16 +0530

    Client side validations working

--- app/models/db_instance.rb
+++ app/models/db_instance.rb
@@ -1,9 +1,12 @@
 class DbInstance < ActiveRecord::Base
-  attr_accessible :identifier, :master_username, :master_password, :allocated_storage, :provision_iops
-  
+  attr_accessible :identifier, :master_username, :master_password, :allocated_storage, :provision_iops,:allocated_storage, :provision_iops
+  attr_accessible :enable_automatic_backup, :backup_window, :maintenance_window, :db_instance_class
+  attr_accessible :db_name, :db_port, :cpu_count, :ram_amount, :backup_retention_period
+
   ## VALIDATIONS ##
-  #validates :identifier, :presence => true
-  #validates :master_username, :presence => true
-  #validates :master_password, :presence => true
-  
-end
+  validates :allocated_storage, :presence => true
+  validates :identifier, :presence => true
+  validates :master_username, :presence => true
+  validates :master_password, :presence => true
+                                                                                                        
+end                                                                                                     
