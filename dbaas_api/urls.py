3aea71b4de8b1dffb280acd9520ae7c9d807aef9
David Newgas <david@geniedb.com>
2013-07-01 15:59:19 -0700

    Auth: allow users to get their token

--- dbaas_api/urls.py
+++ dbaas_api/urls.py
@@ -53,5 +53,6 @@ urlpatterns = patterns('',
     # url(r'^dbaas_api/', include('dbaas_api.foo.urls')),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
     url(r'^admin/', include(admin.site.urls)),
 )
