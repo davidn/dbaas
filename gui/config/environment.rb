599fcb10beaa83f2a659095dba44a2d98bb970e1
David Newgas <david@geniedb.com>
2013-07-03 15:59:00 -0700

    Move gui to subdir

--- gui/config/environment.rb
+++ gui/config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 GenieDb::Application.config.DbaasApiEndpoint = 'http://localhost:8000/api/'
 
 # Auth Token for dbaas-api
-GenieDb::Application.config.DbaasApiToken = 'f156238116b83268e2ab4bcce16e771a412a6df7'
+GenieDb::Application.config.DbaasApiToken = '94c828a98f71e6f46b6e01ac71b0f4c1a31ff33b'
 
 # Initialize the rails application
 GenieDb::Application.initialize!
