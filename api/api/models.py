a4244634d9b8f208ab40b4f3062cbbc66f06e206
David Newgas <david@geniedb.com>
2013-08-06 14:51:50 -0700

    API: Move cloud code into separate module
    Also fix node reference to the connection object

--- api/api/models.py
+++ api/api/models.py
@@ -10,12 +10,11 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from logging import getLogger
-import boto.ec2
 from .route53 import RecordWithHealthCheck, HealthCheck, record, exception
 from boto import connect_route53
 from .uuid_field import UUIDField
 from api.route53 import RecordWithTargetHealthCheck
-import novaclient.v1_1
+from .cloud import EC2, Rackspace
 
 logger = getLogger(__name__)
 
@@ -79,160 +78,13 @@ class Region(models.Model):
     @property
     def connection(self):
         if not hasattr(self, '_connection'):
-            if self.provider.name == "Amazon":
+            if self.provider.code == "az":
                 self._connection = EC2(self.region)
-            elif self.provider.Rackspace == "Rackspace":
+            elif self.provider.code == "rs":
                 self._connection = Rackspace(self.region)
             else:
                 raise KeyError("Unknown provider '%s'" % self.provider.name)
         return self._connection
-
-class EC2(Region):
-    class Meta:
-        proxy = True
-
-    @property
-    def ec2(self):
-        if not hasattr(self, '_ec2'):
-            self._ec2 = boto.ec2.get_region(self.code).connect(aws_access_key_id=settings.AWS_ACCESS_KEY, aws_secret_access_key=settings.AWS_SECRET_KEY)
-        return self._ec2
-
-    def null_or_io1(self, iops):
-        if iops is None:
-            return None
-        else:
-            return 'io1'
-
-    def launch(self, node):
-        # Elastic Block Storage
-        dev_sda1 = boto.ec2.blockdevicemapping.BlockDeviceType(iops=node.iops, volume_type=self.null_or_io1(node.iops))
-        dev_sda1.size = node.storage
-        bdm = boto.ec2.blockdevicemapping.BlockDeviceMapping()
-        bdm['/dev/sda1'] = dev_sda1
-        logger.debug("%s: Assigned NID %s", node, node.nid)
-        sg = self.ec2.create_security_group(str(node),'Security group for '+str(node))
-        node.security_group = sg.id
-        self.ec2.authorize_security_group(
-            group_id=sg.id,
-            ip_protocol='tcp',
-            cidr_ip='0.0.0.0/0',
-            from_port=node.cluster.port,
-            to_port=node.cluster.port)
-        logger.debug("%s: Created Security Group %s (named %s) with port %s open", node, sg.id, sg.name, node.cluster.port)
-        node.save()
-        # EC2 Instance
-        if self.security_group == "":
-            sgs = [sg.name]
-        else:
-            sgs = [sg.name, self.security_group]
-        try:
-            res = self.ec2.run_instances(
-                self.image,
-                key_name=self.key_name,
-                instance_type=node.flavor.code,
-                block_device_map=bdm,
-                security_groups=sgs,
-                user_data ='#include\nhttps://'+Site.objects.get_current().domain+node.get_absolute_url()+'cloud_config/',
-            )
-        except:
-            try:
-                self.ec2.delete_security_group(group_id=node.security_group)
-            except:
-                pass
-            raise
-        node.instance_id = res.instances[0].id
-        logger.debug("%s: Reservation %s launched. Instance id %s", node, res.id, node.instance_id)
-        node.status = node.PROVISIONING
-        node.save()
-
-    def pending(self, node):
-        return self.ec2.get_all_instances(instance_ids=[node.instance_id])[0].instances[0].update() == 'pending'
-
-    def shutting_down(self, node):
-        return self.ec2.get_all_instances(instance_ids=[node.instance_id])[0].instances[0].update() == 'shutting-down'
-
-    def update(self, node, tags={}):
-        instance = self.ec2.get_all_instances(instance_ids=[node.instance_id])[0].instances[0]
-        node.ip = instance.ip_address
-        node.save()
-        for k,v in tags.items():
-            instance.add_tag(k, v)
-
-    def terminate(self, node):
-        self.ec2.terminate_instances([node.instance_id])
-        if node.security_group != "":
-            node.status = Node.SHUTTING_DOWN
-            node.save()
-            while node.shutting_down():
-                sleep(15)
-            logger.debug("%s: terminating security group %s", node, node.security_group)
-            self.ec2.delete_security_group(group_id=node.security_group)
-
-    def pause(self, node):
-        self.ec2.stop_instances([node.instance_id])
-
-    def resume(self, node):
-        self.ec2.start_instances([node.instance_id])
-
-class Openstack(Region):
-    class Meta:
-        proxy = True
-
-    @property
-    def nova(self):
-        if not hasattr(self, "_nova"):
-            self._nova = novaclient.v1_1.client.Client(self.USER,
-                self.PASS,
-                self.TENANT,
-                self.AUTH_URL,
-                service_type="compute",
-                region_name=self.code)
-        return self._nova
-
-    def launch(self, node):
-        server = self.nova.servers.create(
-            name= node.dns_name,
-            image=self.image,
-            flavor=node.flavor.code,
-            key_name=self.key_name,
-            availability_zone=self.code,
-            files={
-                '/var/lib/cloud/seed/nocloud/user-data':'#include\nhttps://'+Site.objects.get_current().domain+node.get_absolute_url()+'cloud_config/',
-                '/var/lib/cloud/seed/nocloud/meta-data':'',
-            },
-        )
-        node.instance_id = server.id
-        node.status=Node.PROVISIONING
-
-    def pending(self, node):
-        return self.nova.servers.get(node.instance_id).status == u'BUILD'
-
-    def shutting_down(self, node):
-        return self.nova.servers.get(node.instance_id).status == u'STOPPING'
-
-    def update(self, node, tags={}):
-        tags['id'] = node.instance_id
-        s = self.nova.servers.set_meta(node.instance_id, tags)
-        node.ip = s.accessIPv4
-        node.save()
-
-    def terminate(self, node):
-        self.nova.servers.delete(node.instance_id)
-
-    def pause(self, node):
-        self.nova.servers.suspend(node.instance_id)
-
-    def resume(self, node):
-        self.nova.servers.resume(node.instance_id)
-
-class Rackspace(Openstack):
-    class Meta:
-        proxy = True
-
-    USER = settings.RACKSPACE_USER
-    PASS = settings.RACKSPACE_PASS
-    TENANT = settings.RACKSPACE_TENANT
-    AUTH_URL = settings.RACKSPACE_AUTH_URL
 
 class Flavor(models.Model):
     provider = models.ForeignKey(Provider, related_name='flavors')
@@ -345,13 +197,13 @@ class Node(models.Model):
         return self.dns_name
 
     def pending(self):
-        return self.connection.pending(self)
+        return self.region.connection.pending(self)
 
     def shutting_down(self):
-        return self.connection.shutting_down(self)
+        return self.region.connection.shutting_down(self)
 
     def update(self, tags={}):
-        return self.connection.update(self, tags)
+        return self.region.connection.update(self, tags)
 
     @models.permalink
     def get_absolute_url(self):
@@ -442,7 +294,7 @@ runcmd:
         logger.debug("%s: Assigned NID %s", self, self.nid)
         logger.info("%s: provisioning node", self)
         try:
-            self.connection.launch(self)
+            self.region.connection.launch(self)
         except:
             self.status = self.ERROR
             self.save()
@@ -479,13 +331,13 @@ runcmd:
 
     def pause(self):
         assert(self.status == Node.RUNNING)
-        self.connection.pause(self)
+        self.region.connection.pause(self)
         self.status = Node.PAUSED
         self.save()
 
     def resume(self):
         assert(self.status == Node.PAUSED)
-        self.connection.resume(self)
+        self.region.connection.resume(self)
         self.status = Node.RUNNING
         self.save()
 
@@ -500,7 +352,7 @@ runcmd:
                 resource_records=[self.ip]))
             rrs.commit()
             r53.delete_health_check(self.health_check)
-            self.connection.terminate(self)
+            self.region.connection.terminate(self)
 
 @receiver(models.signals.pre_delete, sender=Node)
 def node_pre_delete_callback(sender, instance, using, **kwargs):
