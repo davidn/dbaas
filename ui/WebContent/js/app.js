11364fb63e1a38e2f47dfb1ce7af77394f8c303d
Marc Bir <marc@rapidninja.com>
2013-10-08 15:54:04 -0700

    Remove Legacy debug code from app

--- ui/WebContent/js/app.js
+++ ui/WebContent/js/app.js
@@ -1,50 +1,42 @@
-angular.module('GenieDBaaS', ['GenieDBaaS.config', 'Utility.directives', 'GenieDBaaS.services', 'ui.directives', 'ui.bootstrap.tooltipX', 'ngRoute', 'ngSanitize', 'ngResource', 'ngStorage', 'ui.select2', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', "messageBox"]).config(function ($routeProvider, $httpProvider) {
-    $routeProvider.
-        when("/", {templateUrl: 'part/welcome.html', controller: WelcomeCntl}).
-        when("/forgot", {templateUrl: 'part/forgot.html', controller: ForgotCntl}).
-        when("/list", {templateUrl: 'part/list.html', controller: ListCntl}).
-        when("/try", {templateUrl: 'part/try.html', controller: RegisterCntl}).
-        when("/thankyou", {templateUrl: 'part/thanks.html', controller: ThanksCntl}).
-        when("/activate/:activationHash", {templateUrl: 'part/activate.html', controller: ActivationCntl}).
-        when("/quickstart", {templateUrl: 'part/quickstart.html', controller: QuickStartCntl}).
-        when("/cluster", {templateUrl: 'part/cluster.html', controller: ClusterCntl}).
-        when("/cluster/:clusterid/node", {templateUrl: 'part/node.html', controller: NodeCntl}).
-        when("/monitor", {templateUrl: 'part/monitor.html', controller: QuickStartCntl}).
-        when("/logout", {templateUrl: 'part/welcome.html', controller: LogoutCntl}).
-        otherwise({redirectTo: '/'});
+angular.module('GenieDBaaS', ['GenieDBaaS.config', 'Utility.directives', 'GenieDBaaS.services', 'ui.directives', 'ui.bootstrap.tooltipX', 'ngRoute', 'ngSanitize', 'ngResource', 'ngStorage', 'ui.select2', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', "messageBox"])
+    .config(function ($routeProvider, $httpProvider) {
+        $routeProvider.
+            when("/", {templateUrl: 'part/welcome.html', controller: WelcomeCntl}).
+            when("/forgot", {templateUrl: 'part/forgot.html', controller: ForgotCntl}).
+            when("/list", {templateUrl: 'part/list.html', controller: ListCntl}).
+            when("/try", {templateUrl: 'part/try.html', controller: RegisterCntl}).
+            when("/thankyou", {templateUrl: 'part/thanks.html', controller: ThanksCntl}).
+            when("/activate/:activationHash", {templateUrl: 'part/activate.html', controller: ActivationCntl}).
+            when("/quickstart", {templateUrl: 'part/quickstart.html', controller: QuickStartCntl}).
+            when("/cluster", {templateUrl: 'part/cluster.html', controller: ClusterCntl}).
+            when("/cluster/:clusterid/node", {templateUrl: 'part/node.html', controller: NodeCntl}).
+            when("/monitor", {templateUrl: 'part/monitor.html', controller: QuickStartCntl}).
+            when("/logout", {templateUrl: 'part/welcome.html', controller: LogoutCntl}).
+            otherwise({redirectTo: '/'});
 
-    var interceptor = ['$location', '$q', 'growl', function ($location, $q, growl) {
-        function success(response) {
-            return response;
+        var interceptor = function ($location, $q, growl) {
+            function success(response) {
+                return response;
+            };
+
+            function error(response) {
+                if (response.status === 401) {
+                    if ($location.$$path !== '/') {
+                        $location.path("/logout");
+                        growl.error({body: 'Session Expired'});
+                    }
+                }
+                return $q.reject(response);
+            }
+
+            return function (promise) {
+                return promise.then(success, error);
+            }
         };
 
-        function error(response) {
-            if (response.status === 401) {
-                if ($location.$$path !== '/') {
-                    $location.path("/logout");
-                    growl.error({body: 'Session Expired'});
-                }
-            }
-            return $q.reject(response);
-        }
+        $httpProvider.responseInterceptors.push(interceptor);
 
-        return function (promise) {
-            return promise.then(success, error);
-        }
-    }];
-
-    $httpProvider.responseInterceptors.push(interceptor);
-
-    $.fn.sparkline.defaults.common.lineColor = '#8e8e8e';
-    $.fn.sparkline.defaults.common.fillColor = undefined;
-})
-    .config( [
-        '$compileProvider',
-        function( $compileProvider )
-        {
-            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|blob|data):/);
-        }
-    ]);
-
-;
+        $.fn.sparkline.defaults.common.lineColor = '#8e8e8e';
+        $.fn.sparkline.defaults.common.fillColor = undefined;
+    });
 
