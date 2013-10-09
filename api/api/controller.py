92b9fba924b3ea95958bd793644a35556c2bd5b0
David Newgas <david@geniedb.com>
2013-10-09 16:20:51 -0700

    api.controller: simplify imports
    This also fixes use of the wrong launch_cluster.

--- api/api/controller.py
+++ api/api/controller.py
@@ -6,9 +6,7 @@ Created on 9 Oct 2013
 
 from celery import group
 from .models import Node
-from .tasks import install_node, install_region, wait_nodes, \
-    wait_nodes_zabbix, launch_email, complete_pause_node, \
-    complete_resume_node, cluster_ready
+from . import tasks
 
 def launch_cluster(cluster):
     for node in cluster.nodes.all():
@@ -16,22 +14,22 @@ def launch_cluster(cluster):
             node.do_launch()
     install_nodes = cluster.nodes.filter(status=Node.PROVISIONING)
     lbr_regions = cluster.lbr_regions.filter(launched=False)
-    task = launch_cluster.si(cluster) \
-           | wait_nodes.si([node for node in install_nodes]) \
-           | group([install_node.si(node) for node in install_nodes]) \
-           | group([install_region.si(lbr_region) for lbr_region in lbr_regions]) \
-           | wait_nodes_zabbix.si(cluster) \
-           | launch_email.si(cluster) \
-           | cluster_ready.si(cluster)
+    task = tasks.launch_cluster.si(cluster) \
+           | tasks.wait_nodes.si([node for node in install_nodes]) \
+           | group([tasks.install_node.si(node) for node in install_nodes]) \
+           | group([tasks.install_region.si(lbr_region) for lbr_region in lbr_regions]) \
+           | tasks.wait_nodes_zabbix.si(cluster) \
+           | tasks.launch_email.si(cluster) \
+           | tasks.cluster_ready.si(cluster)
     return task.delay()
 
 def pause_node(node):
     node.pause()
-    complete_pause_node.delay(node)
+    tasks.complete_pause_node.delay(node)
 
 def resume_node(node):
     node.resume()
-    complete_resume_node.delay(node)
+    tasks.complete_resume_node.delay(node)
 
 def add_database(cluster, dbname):
     cluster.dbname += ','+dbname
