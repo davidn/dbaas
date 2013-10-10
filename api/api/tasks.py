77d2f0d4c8b3d9e5891d8c79c62f28fdfb3ab317
David Newgas <david@geniedb.com>
2013-10-09 17:04:43 -0700

    api.tasks: celery log to bugsnag if configured

--- api/api/tasks.py
+++ api/api/tasks.py
@@ -13,6 +13,13 @@ from pyzabbix import ZabbixAPI
 
 logger = getLogger(__name__)
 
+if hasattr(settings, 'BUGSNAG'):
+    from celery.signals import after_setup_logger, after_setup_task_logger
+    def add_logger(logger, *args, **kwargs):
+        import bugsnag.handlers
+        logger.addHandler(bugsnag.handlers.BugsnagHandler())
+    after_setup_logger.connect(add_logger)
+    after_setup_task_logger.connect(add_logger)
 
 @task()
 def install_node(node):
