aaedd24e546913139eb420c9ae348294198d8206
David Newgas <david@geniedb.com>
2013-10-11 09:28:05 -0700

    api.crypto: fix public key exporting
    My ridiculous hack for exporting public keys using pyOpenSSL is not
    actually producing valid public keys files (dump_private key is adding
    a version to the asn.1 although versions are onaly added to private keys)
    
    I am switching back to pyCrypto which actually has the ability to handle
    public keys. This has two downsides:
    * We are now using 2 crypto libraries
    * pyCrypto is slower at generating keys.

--- api/api/crypto.py
+++ api/api/crypto.py
@@ -1,37 +1,31 @@
-import base64
-import textwrap
 import datetime
 import OpenSSL
-
-def asn1_to_pem(s, header='RSA PRIVATE KEY'):
-    return "-----BEGIN {0}-----\n{1}\n-----END {0}-----\n".format(header, textwrap.fill(base64.standard_b64encode(s), 64))
+from Crypto.PublicKey import RSA
+from Crypto import Random
 
 class KeyPair(object):
     def __init__(self, key=None):
         if key is None:
-            self._private_key = OpenSSL.crypto.PKey()
-            self._private_key.generate_key(OpenSSL.crypto.TYPE_RSA, 2048)
+            self._private_key = RSA.generate(2048, Random.new().read)
         else:
-            self._private_key = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, key)
+            self._private_key = RSA.importKey(key)
 
     @property
     def private_key(self):
-        return asn1_to_pem(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_ASN1, self._private_key))
+        """Output PEM encoded PKCS1 Private Key"""
+        return self._private_key.exportKey()
 
     @property
     def public_key(self):
-        # this is ridiculous
-        if not hasattr(self,'_public_key'):
-            x = OpenSSL.crypto.X509()
-            x.set_pubkey(self._private_key)
-            self._public_key = x.get_pubkey()
-        return asn1_to_pem(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_ASN1, self._public_key), 'PUBLIC KEY')
+        """Output Pem encoded X509 SubjectPublicKey"""
+        return self._private_key.publickey().exportKey()
 
 class SslPair(KeyPair):
     def __init__(self, CA, CN=None, OU=None, O=None, ST=None, C=None, *args, **kwargs):
         super(SslPair, self).__init__(*args, **kwargs)
+        self._ssl_private_key = OpenSSL.crypto.load_privatekey(self.private_key)
         self._certificate = OpenSSL.crypto.X509()
-        self._certificate.set_pubkey(self._private_key)
+        self._certificate.set_pubkey(self._ssl_private_key)
         if CN is not None:
             self._certificate.get_subject().CN = CN
         if OU is not None:
@@ -47,13 +41,14 @@ class SslPair(KeyPair):
         self._certificate.set_notAfter((datetime.datetime.utcnow() + datetime.timedelta(3650)).strftime('%Y%m%d%H%M%SZ'))
         self._certificate.set_serial_number(2)
         self.certificate_hook()
-        self._certificate.sign(CA._private_key, 'sha1')
+        self._certificate.sign(CA._ssl_private_key, 'sha1')
 
     def certificate_hook(self):
         pass
 
     @property
     def certificate(self):
+        """Output PEM encoded X509 certificate"""
         return OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, self._certificate)
 
 class CertificateAuthority(SslPair):
