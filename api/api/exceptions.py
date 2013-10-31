602ac0f967823770a1db9dcb7c7d446138d8bcc0
David Newgas <david@geniedb.com>
2013-10-30 18:16:16 -0700

    api.exceptions: check_for_salt_error don't have generator in failed
    result has structure:
    { dns_name: { 'out': 'highstate',
                  'ret' : { salt_object: { 'result': bool,
                                           'comment': str,
                                           'changes': dict
                                         },
                            ...
                          },
                },
      ...
    }
    
    We want to only keep salt_objects with 'result':False, and only keep
    dns_names with at least one non-filtered salt_object. We also remove the
    out/ret layer and have dns_name: salt_object.

--- api/api/exceptions.py
+++ api/api/exceptions.py
@@ -20,9 +20,9 @@ class SaltError(Exception):
 
 def check_for_salt_error(result, node_dns_names):
     missing = list(set(node_dns_names) - set(result.iterkeys()))
-    failed = [
-        (dns_name, (change for change in d['ret'] if not change['result']))
-        for dns_name,d in result.iteritems()]
+    failed = (
+        (dns_name, dict((p,c) for p,c in d['ret'].iteritems() if not c['result']))
+        for dns_name,d in result.iteritems())
     failed = dict(f for f in failed if f[1])
     if missing or failed:
         raise SaltError(missing=missing, failed=failed)
