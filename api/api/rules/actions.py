23e5dcb3a5bdcb2eca53aae2c0f6eab328189130
David Newgas <david@geniedb.com>
2013-11-19 11:06:14 -0800

    api.rules: set up some initial rules

--- api/api/rules/actions.py
+++ api/api/rules/actions.py
@@ -1,1 +1,28 @@
-__author__ = 'david'
+from __future__ import unicode_literals
+from datetime import timedelta
+
+
+def disable_account(user):
+    user.is_active = False
+    user.save()
+
+
+def no_cluster_email(user, delta):
+    user.email_user_template('no_cluster_%sday' % timedelta.days, {
+        'username': str(user),
+        'is_paid': user.is_paid,
+        'user': user,
+        'unused_time': delta
+    })
+
+
+def no_cluster_email_2days(user):
+    no_cluster_email(user, timedelta(days=2))
+
+
+def no_cluster_email_5days(user):
+    no_cluster_email(user, timedelta(days=5))
+
+
+def no_cluster_email_10days(user):
+    no_cluster_email(user, timedelta(days=10))
--- api/api/rules/conditions.py
+++ api/api/rules/conditions.py
@@ -1,1 +1,42 @@
-__author__ = 'david'
+from django.conf import settings
+from datetime import datetime, timedelta
+from ..models import Cluster
+
+
+def user_expired(user):
+    if user.is_paid:
+        return False
+    q = Cluster.history.filter(user_id=user.id, status=Cluster.PROVISIONING).order_by('-history_date')
+    try:
+        first_cluster = q[0]
+    except IndexError:
+        return False
+    return first_cluster.history_date > settings.TRIAL_LENGTH
+
+
+def user_launched(user):
+    return Cluster.history.filter(user_id=user.id).count() != 0
+
+
+def user_not_launched(user):
+    return not user_launched(user)
+
+
+def user_not_launched_after_2days(user):
+    return user.date_joined + timedelta(days=2) < datetime.now() and not user_launched(user)
+
+
+def user_not_launched_after_5days(user):
+    return user.date_joined + timedelta(days=5) < datetime.now() and not user_launched(user)
+
+
+def user_not_launched_after_10days(user):
+    return user.date_joined + timedelta(days=10) < datetime.now() and not user_launched(user)
+
+
+def tables_created(user):
+    return False
+
+
+def rows_created(user):
+    return False
